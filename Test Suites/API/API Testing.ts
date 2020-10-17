deabaea36f41420563ccf0d877ca33d5ded69cbb
khaidarfp <khaidar.febrianto@gmail.com>
2020-10-17 10:36:39 +0700

    penambahan test case

--- Test Suites/API/API Testing.ts
+++ Test Suites/API/API Testing.ts
@@ -12,6 +12,30 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>65232f06-5a21-4ce8-ac9a-38941b5b4142</testSuiteGuid>
    <testCaseLink>
+      <guid>08428d76-9c2e-469f-abde-56eedbad47ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Testing/TC_API_POST_Register_Unsuccessful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>616e0315-7b8f-4bfd-98cf-1bff65b89c38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Testing/TC_API_POST_Register_Successful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed7a4d03-c6e0-4a3c-87e1-3233e8107c8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Testing/TC_API_POST_Login_Unsuccessful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>218a3654-8192-4930-bf2e-cfc87ba3e7be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Testing/TC_API_POST_Login_Successful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
       <guid>6bf068c8-a772-4f0b-92d4-5266955ebf5b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -28,5 +52,11 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API_Testing/TC_API_DELETE_Delete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f1419ee2-eeca-4bb1-9615-89b3bf4bd631</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Testing/TC_API_GET_Delay_Respon</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
